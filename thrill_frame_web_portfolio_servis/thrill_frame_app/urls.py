4f418c76922251f62578f629a27732fb755ae39b
Arseniy Petrenko <arsfix2004@gmail.com>
2026-09-08 12:50:32 +0300

    Refactor navbar design and fix URL routing

--- thrill_frame_web_portfolio_servis/thrill_frame_app/urls.py
+++ thrill_frame_web_portfolio_servis/thrill_frame_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index
+from . import views
 
 
 app_name = "thrill_frame_app"
 
 urlpatterns = [
-    path("", index, name="index")
+    path('', views.index, name='home'),
+    path('video/', views.index, name='video'),
+    path('photos/', views.index, name='photos'),
+    path('contacts/', views.index, name='contacts'),
 ]
